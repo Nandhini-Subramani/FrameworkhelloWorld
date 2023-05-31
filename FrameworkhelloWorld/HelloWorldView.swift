863989d45431693ddf147e3f3f307ed12bb7c87a
Nandhini-Subramani <Nandhini.Subramani.Contractor@pepsico.com>
2023-05-31 18:13:20 +0530

    Add proj and xcframework hello world

---
+++ FrameworkhelloWorld/HelloWorldView.swift
@@ -0,0 +1,21 @@
+//
+//  HelloWorldView.swift
+//  FrameworkhelloWorld
+//
+//  Created by Nandhini Subramani on 31/05/23.
+//
+
+import SwiftUI
+
+public struct HelloWorldView: View {
+    public init() {}
+    public var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HelloWorldView_Previews: PreviewProvider {
+    static var previews: some View {
+        HelloWorldView()
+    }
+}
---
+++ build/FrameworkhelloWorld.framework-catalyst.xcarchive/Products/Library/Frameworks/FrameworkhelloWorld.framework/Versions/A/Headers/FrameworkhelloWorld.h
@@ -0,0 +1,18 @@
+//
+//  FrameworkhelloWorld.h
+//  FrameworkhelloWorld
+//
+//  Created by Nandhini Subramani on 31/05/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FrameworkhelloWorld.
+FOUNDATION_EXPORT double FrameworkhelloWorldVersionNumber;
+
+//! Project version string for FrameworkhelloWorld.
+FOUNDATION_EXPORT const unsigned char FrameworkhelloWorldVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FrameworkhelloWorld/PublicHeader.h>
+
+
